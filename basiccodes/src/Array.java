a047df1730160f1383a9dcaf74daf01a124deaa2
unknown <amanwish2001@gmail.com>
2022-07-28 01:21:31 +0530

    modified array.java

---
+++ basiccodes/src/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String[] args) {
+        int [] marks = new int[5];
+        marks[0] = 13;
+        marks[1]= 10;
+        marks[2]=50;
+
+        for (int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+
+        }
+    }
+}
