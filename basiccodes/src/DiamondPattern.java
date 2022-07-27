f1e175e3c4ee819a36652b829c730d8a38dce6d0
unknown <amanwish2001@gmail.com>
2022-07-28 01:13:38 +0530

    modified some files

--- basiccodes/src/DiamondPattern.java
+++ basiccodes/src/DiamondPattern.java
@@ -2,13 +2,16 @@ public class DiamondPattern {
     public static void main(String[] args) {
         int row = 4;
 
-        for (int i=row; i>=1;i--){
-            for (int j=1; j<i;j++){
-                System.out.println(" ");
+        for (int i=1; i<=row;i++){
+            for (int j=1; j<=row-i;j++){
+                System.out.print("-");
             }
-            for (int j=1;j<=row-i;j++){
-                System.out.println("*");
+            for (int j=1;j<row-i;j++){
+                if (j%2==1)
+                System.out.print("*");
             }
+            System.out.println();
         }
+
     }
 }
